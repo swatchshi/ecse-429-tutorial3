907a3bfb51f67c7429d7130df843d414fbfdb59d
swatchshi <swatchshi@gmail.com>
2018-09-24 15:45:35 -0400

    first commit

---
+++ src/test/java/ca/mcgill/ecse429/AppTest.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse429;
+
+
+//import org.junit.framework.TestCase;
+//import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+
+import org.junit.Test;
+
+public class AppTest
+{
+    @Test
+    public void emptyTest() {
+
+    }
+}
